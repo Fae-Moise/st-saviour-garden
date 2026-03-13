ac93c46e8c9f41c3c15c49fc614f379e44f84fa8
fmoise <fmoise28@stsaviour.org>
2026-03-13 12:35:00 -0400

    garden.py

--- garden.py
+++ garden.py
@@ -40,3 +40,14 @@ for i in range(11):
             else:
                 print('🌸', end='')
         print('')
+print('plaid')
+for i in range(11):
+        for j in range(11):
+            if j % 2== 0 and i % 2== 0:
+                print('🌸', end='')
+            else:
+                print('🌺', end='')
+        print('')
+
+
+
